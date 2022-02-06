09e414fe01ff82591fd2021df2141dcd242ab15d
Peterson-moura <peterson.moura@outlook.com.br>
2022-02-05 23:28:15 -0300

    _Login_Fake_apk

---
+++ app/src/main/java/com/example/loginfake_peterson_moura/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.example.loginfake_peterson_moura.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
